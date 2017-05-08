9ea1bc2d72df104c2ce60b4180db141d81538a9e
Ricky Aguayo <raguayo911@gmail.com>
2017-05-08 13:17:40 -0500

    initial commit

---
+++ js_fund/test.js
@@ -0,0 +1,10 @@
+// Go through each value in the array x, where x = [3,5,"Dojo", "rocks", "Michael", "Sensei"]. Log each value.
+
+var x = [3,5,"Dojo", "rocks", "Michael", "Sensei"]
+for val in x:
+	console.log(val)
+// Add a new value (100) in the array x using a push method.
+// Add a new array ["hello", "world", "JavaScript is Fun"] to variable x. Log x in the console and analyze how x looks now. 
+// Create a simple for loop that sums all the numbers between 1 to 500. Have console log the final sum. 
+// Write a loop that will go through the array [1, 5, 90, 25, -3, 0], find the minimum value, and then print it 
+// Write a loop that will go through the array [1, 5, 90, 25, -3, 0], find the average of all of the values, and then print 
