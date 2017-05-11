9e74803564672f4359c0edb15dad3a30da39fae9
Ricky Aguayo <raguayo911@gmail.com>
2017-05-11 13:41:37 -0500

    stuff

--- node/test/apps.js
+++
@@ -1,7 +0,0 @@
-var http = require('http')
-var fs = require('fs')
-var server = http.createServer(function (request, response){
-	console.log('client request URL:', request.url);
-})
-
-console.log(server)
